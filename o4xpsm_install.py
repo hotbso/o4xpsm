11594ca5070cf50db1f0420abc2240204ad4938e
Holger Teutsch <holger.a.teutsch@gmail.com>
2023-08-04 18:14:14 +0200

    improve logging

--- o4xpsm_install.py
+++ o4xpsm_install.py
@@ -24,6 +24,8 @@
 import sys, os, shlex, subprocess
 import logging
 
+log = logging.getLogger("o4xpsm_install")
+
 seasons = ["win", "spr", "sum", "fal"]
 sh_zones = ["vcld", "cld", "tmp", "wrm", "vhot"]
 
@@ -93,39 +95,39 @@ def parse_scenery_packs():
                 path = ' '.join(w[1:])
 
                 if path.find("simHeaven_X-World_Vegetation_Library") != -1:
-                    logging.info("simHeaven_X-World_Vegetation_Library detected")
+                    log.info("simHeaven_X-World_Vegetation_Library detected")
                     global simheaven_path
                     simheaven_path = path
 
                 if path.find("Global_Forests_v2") != -1:
-                    logging.info("Global_Forests_v2 detected")
+                    log.info("Global_Forests_v2 detected")
                     global gfv2_path
                     gfv2_path = path
 
 def check_and_link(name, path):
-    logging.debug(f"{name} -> {path}")
+    log.debug(f"{name} -> {path}")
 
     if os.path.isdir(name):
-        logging.info(f"{name} is already linked")
+        log.info(f"{name} is already linked")
         if os.path.isfile(os.path.join(name, "library.txt")):
-            logging.info(f"link {name} is valid")
+            log.info(f"link {name} looks valid")
         else:
-            logging.error(f"link {name} is invalid! Check!")
+            log.error(f"link {name} is invalid! Check!")
             exit(1)
 
     else:
         if not os.path.isabs(path):
             path = os.path.join("..", "..", path)
-            logging.info(f"path: {path}")
+            log.info(f"path: {path}")
 
         if sys.platform == "win32":
             # os.symlink needs Administrator rights so create a junction
-            logging.info(f"creating junction {name} -> {path}")
+            log.info(f"creating junction {name} -> {path}")
             path = os.path.abspath(path)
-            #logging.info(f"path: {path}")
+            #log.info(f"path: {path}")
             out = subprocess.run(shlex.split(f'mklink /j "{name}" "{path}"'), shell = True)
             if out.returncode != 0:
-                logging.error(f"Can't create junction: {out}")
+                log.error(f"Can't create junction: {out}")
                 exit(1)
         else:
             os.symlink(path, name)
@@ -137,20 +139,22 @@ logging.basicConfig(level=logging.INFO,
                     handlers=[logging.FileHandler(filename = "o4xpsm_install.log", mode='w'),
                               logging.StreamHandler()])
 
-logging.info(f"args: {sys.argv}")
+log.info(f"args: {sys.argv}")
 debug = "-debug" in sys.argv
 if debug:
-    logging.info("Debug mode enabled")
+    log.info("Debug mode enabled")
 
 install_dir = os.path.basename(os.path.normpath(os.path.join(os.getcwd(), '..')))
 if install_dir != 'Custom Scenery':
-    logging.error("Installation error, must be installed in 'Custom Scenery'")
+    log.error("Installation error, must be installed in 'Custom Scenery'")
     exit(1)
 
-logging.info("Parsing scenery_packs.ini")
+log.info("")
+log.info("Parsing scenery_packs.ini")
 parse_scenery_packs()
 
-logging.info("Checking links")
+log.info("")
+log.info("Checking links")
 check_and_link("1200_forests", os.path.abspath("../../Resources/default scenery/1200 forests"))
 
 if simheaven_path is not None:
@@ -159,7 +163,8 @@ if simheaven_path is not None:
 if gfv2_path:
     check_and_link("Global_Forests_v2", gfv2_path)
 
-logging.info("Creating library.txt")
+log.info("")
+log.info("Creating library.txt")
 out = open("library.txt", "w")
 
 
@@ -193,20 +198,20 @@ for z in sh_zones:
         out.write(f"REGION_BITMAP simHeaven_X-World_Vegetation_Library/Maps/climate_zone_{z}.png\n")
         out.write(f"REGION_DREF o4xpsm/{s} == 1\n")
 
-logging.info("Processing XP12 native forests")
+log.info("Processing XP12 native forests")
 season = parse_lib("1200_forests/library.txt")
 write_seasons(season, "o4xpsm_", "1200_forests")
 
 if gfv2_path is not None:
-    logging.info("Processing Global_Forests_v2")
+    log.info("Processing Global_Forests_v2")
     season = parse_lib("Global_Forests_v2/library.txt")
     write_seasons(season, "o4xpsm_", "Global_Forests_v2")
 
 if simheaven_path is not None:
-    logging.info("Processing simHeaven_X-World_Vegetation_Library")
+    log.info("Processing simHeaven_X-World_Vegetation_Library")
     for z in sh_zones:
         season = parse_lib("simHeaven_X-World_Vegetation_Library/library.txt", "climate_zone_" + z)
         write_seasons(season, f"o4xpsm_sh_{z}_", "simHeaven_X-World_Vegetation_Library")
 
 out.close()
-logging.info("Success!")
+log.info("Success!")
